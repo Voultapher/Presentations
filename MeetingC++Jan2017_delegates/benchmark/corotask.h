98630efea3453bf5107c66403fb21b6514d5eb08
Voultapher <lukas.bergdoll@gmail.com>
2017-01-18 18:10:04 +0100

    Renamed Utility to corotask

--- MeetingC++Jan2017_delegates/benchmark/corotask.h
+++ MeetingC++Jan2017_delegates/benchmark/corotask.h
@@ -2,7 +2,6 @@
 #define COROUTINEVSFUNCTION_UTILITY_H_INCLUDED
 
 #include <experimental\coroutine>
-#include <variant>
 
 using namespace std::experimental;
 
@@ -70,5 +69,40 @@ namespace util
 	};
 
 }
+
+template<typename T> class coro_del; // unspecified
+
+template<typename R, typename... Args> class coro_del<R(Args...)>
+{
+public:
+	explicit coro_del() {}
+
+	template<typename F> coro_del(F&& func)
+	{
+		task_ = [](auto func, coro_del* ob) -> util::coro_task
+		{
+			co_await std::experimental::suspend_always{};
+			func(ob->arg);
+		}(std::forward<F>(func), this);
+	}
+
+	coro_del(const coro_del&) = default;
+	coro_del(coro_del&&) = default;
+
+	coro_del& operator= (const coro_del&) = default;
+	coro_del& operator= (coro_del&&) = default;
+
+	~coro_del() = default;
+
+	R operator() (int user_arg)
+	{
+		arg = user_arg;
+		task_.resume();
+		return 3; //tmp
+	}
+private:
+	util::coro_task task_;
+	int arg; // hack, should be tuple and aplly
+};
 
 #endif // COROUTINEVSFUNCTION_UTILITY_H_INCLUDED
