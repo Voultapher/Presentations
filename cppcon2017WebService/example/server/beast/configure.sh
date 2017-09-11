b01c3fc3bb989adc91b3ba7518f40fc5593a673a
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2017-09-11 16:08:07 +0200

    Update beast example build dependecies

--- cppcon2017WebService/example/server/beast/configure.sh
+++ cppcon2017WebService/example/server/beast/configure.sh
@@ -9,7 +9,7 @@ mkdir -p "${SCRIPT_PATH}/Debug"
 cd "${SCRIPT_PATH}/Debug"
 cmake -G Ninja \
   -DCMAKE_CXX_COMPILER=clang++ \
-  -DCMAKE_CXX_FLAGS="-Wall -Wextra" \
+  -DCMAKE_CXX_FLAGS="-Wall -Wextra -stdlib=libc++" \
   -DCMAKE_BUILD_TYPE=Debug \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 
@@ -17,5 +17,6 @@ mkdir -p "${SCRIPT_PATH}/Release"
 cd "${SCRIPT_PATH}/Release"
 cmake -G Ninja \
   -DCMAKE_CXX_COMPILER=clang++ \
+  -DCMAKE_CXX_FLAGS="-stdlib=libc++" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
