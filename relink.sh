c1fc46c162fceb294aac71f150a145f2f4183934
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2020-01-24 12:15:01 +0100

    Unify asset location rendering

--- relink.sh
+++ relink.sh
@@ -5,8 +5,11 @@ set -euxo pipefail
 rm -f index.html
 ln -s ../_style/index.html index.html
 
+rm -f present.md
+ln -s "${1}/present.md" present.md
+
 rm -f style
 ln -s ../_style style
 
 rm -f assets
-ln -s "${1}" assets
+ln -s "${1}/assets" assets
